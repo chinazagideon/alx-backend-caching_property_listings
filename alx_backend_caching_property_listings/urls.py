1cca2e451bf779495cd91876218a7a804f6bb663
chinazagideon <chinazagideon264@gmail.com>
2025-08-31 20:51:52 +0100

    rename project

--- alx_backend_caching_property_listings/urls.py
+++ alx_backend_caching_property_listings/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for alxbackendcachingpropertylistings project.
+URL configuration for alx_backend_caching_property_listings project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/

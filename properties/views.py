6f1ca163243761d1359e1fb19b1e5560e9dea6b9
chinazagideon <chinazagideon264@gmail.com>
2025-08-31 21:25:01 +0100

    implement page caching

--- properties/views.py
+++ properties/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from .models import Property
 
-# Create your views here.
+"""
+Set default page cache timeout
+"""
+@cache_page(60 * 15)
+def property_list(request):
+    """
+    A view to display all the properties
+    """
+    properties = Property.objects.all()
+    return render(request, 'properties/property_list.html', {'properties', properties})

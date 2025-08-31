9220465df544165aba4ff50ec3c77fc6518c8b20
chinazagideon <chinazagideon264@gmail.com>
2025-08-31 21:51:00 +0100

    cache metrics

--- properties/utils.py
+++ properties/utils.py
@@ -1,6 +1,48 @@
 from django.core.cache import cache
 from .models import Property
 import os
+import logging
+from django_redis import get_redis_connection
+from django.core.cache import cache
+
+
+def get_redis_cache_metrics():
+    """
+    Retrieves and logs cache hit/miss metrics from Redis 
+    """
+
+    #get redis lowlevel connection client
+    client = get_redis_connection("default")
+
+    #get server information
+    info = client.info('keyspace')
+
+    # keyspace_hits and keyspace_misses
+    try:
+        keyspace_hits = int(info['keyspace_hits'])
+        keyspace_misses = int(info['keyspace_misses'])
+        total_requests = keyspace_misses + keyspace_hits
+
+        hit_ratio = 0
+        if total_requests > 0 :
+            hit_ratio = (keyspace_hits / total_requests) * 100
+    except (KeyError, ValueError):
+        hit_ratio = 0
+        total_requests = 0
+        keyspace_hits = 0
+        keyspace_misses = 0
+    
+    #log the metrics
+    logging.info(f'Redis cache Metrics:')
+    logging.info(f"  - Keyspace Hits: {keyspace_hits}")
+    logging.info(f"  - Keyspace Misses: {keyspace_misses}")
+    logging.info(f"  - Hit Ratio: {hit_ratio:.2f}%" if isinstance(hit_ratio, float) else f"  - Hit Ratio: {hit_ratio}")
+
+    return {
+        'keyspace_hits': keyspace_hits,
+        'keyspace_misses': keyspace_misses,
+        'hit_ratio': hit_ratio
+    }
 
 def get_all_properties():
     """
